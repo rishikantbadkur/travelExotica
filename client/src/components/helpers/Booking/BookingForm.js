9fa4d581a3967d7e9c5bb6116cf5dce39a4937d9
Rishikant Badkur <badkurrishi@gmail.com>
2024-09-24 12:11:33 +0530

    fix UI changes for test payment

--- client/src/components/helpers/Booking/BookingForm.js
+++ client/src/components/helpers/Booking/BookingForm.js
@@ -170,7 +170,7 @@ const BookingForm = ({ tour }) => {
         {booking.adult * tour.price +
           booking.children * Math.floor(tour.price * 0.6) >
           50000 && (
-          <p>
+          <p style={{ fontSize: "1.5rem", color: "red", lineHeight: "2rem" }}>
             <span style={{ fontWeight: "bolder" }}>Note :</span> Payment amount
             in test mode should be less than{" "}
             {new Intl.NumberFormat("en-IN", {
